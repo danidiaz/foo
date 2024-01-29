69625e8d292562de4e17129ad914a6cb88e3fd78
Daniel Díaz <diaz_carrete@yahoo.com>
2024-01-29 06:16:56 +0100

    change name of output derivation

--- flake.nix
+++ flake.nix
@@ -5,11 +5,11 @@
 
   outputs = { self, nixpkgs }: {
 
-    packages.x86_64-linux.hello = 
+    packages.x86_64-linux.foo = 
       let myDeriv = nixpkgs "./foo.nix" {};
        in nixpkgs.legacyPackages.x86_64-linux.hello;
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    packages.x86_64-linux.default = self.packages.x86_64-linux.foo;
 
   };
 }
