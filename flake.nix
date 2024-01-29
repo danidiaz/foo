49675140d5949611eec926b8b8b626b817bb8a5c
Daniel Díaz <diaz_carrete@yahoo.com>
2024-01-29 06:28:44 +0100

    ...

--- flake.nix
+++ flake.nix
@@ -6,8 +6,8 @@
   outputs = { self, nixpkgs }: {
 
     packages.x86_64-linux.foo = 
-      let myDeriv = nixpkgs "./foo.nix" {};
-       in nixpkgs.legacyPackages.x86_64-linux.hello;
+      let myDeriv = nixpkgs.haskellPackages.callPackage ./foo.nix {};
+       in myDeriv;
 
     packages.x86_64-linux.default = self.packages.x86_64-linux.foo;
 
